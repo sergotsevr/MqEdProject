c709198774344db9802c11dc827e300c75861720
sgotsev <sgotsev@cinimex.ru>
2022-05-06 13:01:45 +0300

    read and refactor

--- src/main/java/com/configuration/rabbirmq/RabbitConfiguration.java
+++ src/main/java/com/configuration/rabbirmq/RabbitConfiguration.java
@@ -1,12 +1,16 @@
 package com.configuration.rabbirmq;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +22,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfiguration {
 
     @Value("${rabbit.host}")
-    String host;
+    private String host;
 
-    @Value("${rabbit.queue}")
-    String queue;
+   // @Value("${rabbit.queue}")
+    private final String QUEUE = "queue1";
     @Bean
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(host);
@@ -39,7 +43,26 @@ public class RabbitConfiguration {
 
     @Bean
     public Queue myQueue() {
-        return new Queue(queue);
+        return new Queue(QUEUE);
     }
+
+   /* @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }*/
 
 }
--- src/main/java/com/service/RabbitService.java
+++ src/main/java/com/service/RabbitService.java
@@ -1,27 +1,45 @@
 package com.service;
 
 import com.configuration.rabbirmq.RabbitConfiguration;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.Message;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Service
+@Slf4j
 @ConditionalOnBean(RabbitConfiguration.class)
 public class RabbitService implements QueueService{
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    Message message = new Message("Init rabbitmq message");
 
     @Autowired
     AmqpTemplate template;
 
     @Override
     public Message readMessage() {
-        return null;
+        return message;
     }
     @Override
+    @SneakyThrows
     public void writeMessage(Message message) {
-        template.convertAndSend(message.getMessageText().getBytes(StandardCharsets.UTF_8));
+        template.convertAndSend("queue1", objectMapper.writeValueAsString(message));
+    }
+
+    @RabbitListener(queues = "queue1")
+    public void processQueue1(String message) throws IOException {
+        log.info("Received from queue 1: " + message);
+        //this.message = new Message(message);
+        this.message = objectMapper.readValue(message, Message.class);
     }
 }
