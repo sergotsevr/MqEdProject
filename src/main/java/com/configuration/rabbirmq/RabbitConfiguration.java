28fe91345f5f05c1d35249fe388d58290649864b
sgotsev <sgotsev@cinimex.ru>
2022-05-06 13:03:27 +0300

    refactor

--- src/main/java/com/configuration/rabbirmq/RabbitConfiguration.java
+++ src/main/java/com/configuration/rabbirmq/RabbitConfiguration.java
@@ -1,16 +1,12 @@
 package com.configuration.rabbirmq;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -24,8 +20,9 @@ public class RabbitConfiguration {
     @Value("${rabbit.host}")
     private String host;
 
-   // @Value("${rabbit.queue}")
-    private final String QUEUE = "queue1";
+    @Value("${rabbit.queue}")
+    private String queue = "queue1";
+
     @Bean
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(host);
@@ -43,26 +40,7 @@ public class RabbitConfiguration {
 
     @Bean
     public Queue myQueue() {
-        return new Queue(QUEUE);
+        return new Queue(queue);
     }
-
-   /* @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(QUEUE);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    Receiver receiver() {
-        return new Receiver();
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }*/
 
 }
