167c7ee6767c4f9b6bf37a7e565bdd3125fbbdea
sgotsev <sgotsev@cinimex.ru>
2022-05-12 13:50:10 +0300

    add test

---
+++ src/test/java/com/kafka/SpeedTest.java
@@ -0,0 +1,39 @@
+package com.kafka;
+
+import com.configuration.kafka.KafkaConfiguration;
+import com.model.Message;
+import com.service.QueueService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application.yml")
+public class SpeedTest {
+    @Autowired
+    QueueService service;
+
+    @Test
+    public void speedTest() {
+        Message message = prepareMessage();
+        long startTime = System.currentTimeMillis();
+        service.writeMessage(message);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime) + "ms");
+    }
+
+    private Message prepareMessage() {
+        return new Message("Test message");
+    }
+
+    @Bean
+    KafkaConfiguration kafkaConfiguration(){
+        return new KafkaConfiguration();
+    }
+}
