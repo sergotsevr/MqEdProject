e6c4839ad63e73779be58f026b4be4d09e733662
sgotsev <sgotsev@cinimex.ru>
2022-05-24 13:42:43 +0300

    add testcontainer for kafka

--- src/test/java/com/kafka/KafkaSpeedTest.java
+++ src/test/java/com/kafka/KafkaSpeedTest.java
@@ -1,18 +1,35 @@
 package com.kafka;
 
 import com.AbstractSpeedTest;
+import com.configuration.kafka.KafkaConfiguration;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-//раскоментировать для тестирования без поднятия кафки
-//@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@Import(KafkaConfiguration.class)
 @TestPropertySource(
         locations = "classpath:application.yml", properties = "mq.type = kafka")
 public class KafkaSpeedTest extends AbstractSpeedTest {
 
+    @ClassRule
+    public static KafkaContainer kafka;
+
+    static {
+        kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+        kafka.start();
+    }
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("kafka.bootstrap-servers",kafka::getBootstrapServers);
+    }
 }
